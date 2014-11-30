9a4d2baa18c8f42a8b96389950aa34ef34e97d07
Karl Pietrzak <kpietrzak@mimeya.com>
2014-11-29 20:41:27 -0500

    now supports ipv6 by using google's guava
    didn't want to use Java's InetAddress to avoid DNS lookups

---
+++ src/main/java/com/github/thealchemist/pg_hibernate/InetAddressType.java
@@ -0,0 +1,106 @@
+package com.github.thealchemist.pg_hibernate;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.usertype.UserType;
+import org.postgresql.util.PGobject;
+
+import com.google.common.net.InetAddresses;
+
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.StringTokenizer;
+
+/**
+ * A Hibernate <b>UserType</b> for PostgreSQL's <b>inet</b> type.
+ *
+ * @author Jesse Costello-Good, The Alchemist
+ */
+public class InetAddressType implements UserType {
+
+	@Override
+    public int[] sqlTypes() {
+		return new int[]{java.sql.Types.OTHER};
+	}
+
+	@Override
+    public Class<InetAddress> returnedClass() {
+		return InetAddress.class;
+	}
+
+	@Override
+    public boolean equals( Object o, Object o1 ) throws HibernateException {
+		if (o == null && o1 == null)
+			return true;
+		else if (o == null || o1 == null)
+			return false;
+		return o.equals(o1);
+	}
+
+	 @Override
+	 public Object nullSafeGet(ResultSet resultSet, String[] strings, SessionImplementor sessionImplementor, Object o) throws HibernateException, SQLException {
+
+		if (strings.length != 1)
+			throw new IllegalArgumentException("strings.length != 1, strings = " + strings);
+
+		String value = resultSet.getString(strings[0]);
+
+		if (value == null) {
+			return null;
+		} else {
+			return InetAddresses.forString(value);
+		}
+	}
+
+    @Override
+    public void nullSafeSet(PreparedStatement preparedStatement, Object o, int i, SessionImplementor sessionImplementor) throws HibernateException, SQLException {
+
+		if (o == null) {
+			preparedStatement.setNull(i, java.sql.Types.OTHER);
+		} else {
+			PGobject object = new PGobject();
+			object.setValue(((InetAddress) o).getHostAddress());
+			preparedStatement.setObject(i, object);
+		}
+	}
+
+	@Override
+    public Object deepCopy( Object o ) throws HibernateException {
+		if (o == null) return null;
+		try {
+			return InetAddress.getByAddress(((InetAddress) o).getAddress());
+		}
+		catch (UnknownHostException e) {
+			throw new AssertionError("this can't happen!");
+		}
+	}
+
+	@Override
+    public boolean isMutable() {
+		return false;
+	}
+
+    @Override
+    public int hashCode(Object o) throws HibernateException {
+        return o.hashCode();
+    }
+
+    @Override
+    public Serializable disassemble(Object o) throws HibernateException {
+        return (Serializable) o;
+    }
+
+    @Override
+    public Object assemble(Serializable cached, Object owner) throws HibernateException {
+        return cached;
+    }
+
+    @Override
+    public Object replace(Object original, Object target, Object owner) throws HibernateException {
+        return original;
+    }
+}
